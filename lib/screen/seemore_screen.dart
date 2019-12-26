7622aae5a49f98bf16bf46fcd77c1cd544f29724
Rout <devs.lecture28@gmail.com>
2019-12-26 23:56:47 +0700

    done more screen interface

--- lib/screen/seemore_screen.dart
+++ lib/screen/seemore_screen.dart
@@ -25,36 +25,65 @@ class SeemoreScreen extends StatelessWidget {
           child: ListView.builder(
               itemCount: courseList.length,
               itemBuilder: (BuildContext context, int index) {
-                return Stack(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      height: 150,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(2, 4),
-                                color: Colors.black26,
-                                blurRadius: 3),
-                          ]),
-                    ),
-                    Container(
-                      height: 150,
-                      width: 110,
-                      margin: EdgeInsets.only(
-                        left: 25,
+                return Container(
+                  margin:
+                  EdgeInsets.symmetric(vertical: 7, horizontal: 10),
+                  height: 160,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(2, 3),
+                            color: Colors.black12,
+                            blurRadius: 2),
+                      ]),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(3),bottomLeft: Radius.circular(3)),
+                            image: DecorationImage(
+                                image: AssetImage(courseList[index].imgUrl),
+                                fit: BoxFit.cover)),
                       ),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          image: DecorationImage(
-                              image: AssetImage(courseList[index].imgUrl),
-                              fit: BoxFit.cover)),
-                    )
-                  ],
+                      Padding(
+                        padding:  EdgeInsets.symmetric(vertical: 20,horizontal: 15),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              width:240,
+                              child: Text(
+                                courseList[index].title.length > 50 ? courseList[index].title.substring(1,50) : courseList[index].title,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'CM Sans Serif',
+                                  fontSize: 16,
+                                  height: 1.5
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 5,),
+                            Container(
+                              width:240,
+                              child: Text(
+                                courseList[index].description.length > 150 ? courseList[index].description.substring(1,150) : courseList[index].description,
+                                style: TextStyle(
+                                    fontFamily: 'CM Sans Serif',
+                                    fontSize: 12,
+                                    height: 1.3,
+                                  color: Colors.black54
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 );
               }),
         ),
